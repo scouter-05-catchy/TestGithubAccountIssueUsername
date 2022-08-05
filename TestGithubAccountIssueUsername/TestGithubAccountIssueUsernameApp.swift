89d98a075b6a6680507a8d566a0bfa3a3b01708f
Daniel Yount <danielyount@apple.com>
2022-08-05 14:25:11 -0700

    Initial Commit

---
+++ TestGithubAccountIssueUsername/TestGithubAccountIssueUsernameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubAccountIssueUsernameApp.swift
+//  TestGithubAccountIssueUsername
+//
+//  Created by Daniel Yount on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubAccountIssueUsernameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
